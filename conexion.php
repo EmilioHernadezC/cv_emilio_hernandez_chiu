c33f66c4cd539cd57c13bd93bb7728d775b7a6fe
Emilio Hernandez <emixrc@gmail.com>
2021-09-17 14:16:41 -0500

    conexion actualizado

--- conexion.php
+++ conexion.php
@@ -4,21 +4,10 @@
     $servidor = "localhost"; 
     $basededatos ="curriculum"; 
             
-    $conexion = mysqli_connect  ($servidor,$usuario,"") or die ("Error con el servidor de la Base de datos");             
-    $db = mysqli_select_db($conexion, $basededatos) or die ("Error conexion al conectarse a la Base de datos");
+    $conexion = mysqli_connect  ($servidor,$usuario,$password,$basededatos);             
+    mysqli_query($conexion, "insert into datos(nombre, email, asunto, mensaje) values
+        ('$_REQUEST[nombre]','$_REQUEST[email]','$_REQUEST[asunto]', '$_REQUEST[mensaje]')")
+        or die("Problemas en el select" . mysqli_error($connection));
+    mysqli_close($conexion); 
     
-    $nombre=$_POST['nombre']; 
-    $correo=$_POST['email'];
-    $correo=$_POST['asunto'];  
-    $mensaje=$_POST['mensaje'];
-  
-    $sql="INSERT INTO datos VALUES ('$nombre','$correo','$mensaje')"; 
-              
-    $ejecutar=mysqli_query($conexion, $sql);
-       
-    if(!$ejecutar){
-        echo"hubo algun error";
-    }else{
-        echo"datos guardado correctamente <br><a href='index.html'>volver</a>"; 
-    }
 ?>
