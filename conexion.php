21d215f4b73009f3c4ada2c1d344298e1cc6d58c
Emilio Hernandez <emixrc@gmail.com>
2021-09-17 17:14:15 -0500

    conexion actualizado

--- conexion.php
+++ conexion.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="estilos.css">
 <?php
     $usuario = "root";
     $password = "";   
@@ -9,5 +10,13 @@
         ('$_REQUEST[nombre]','$_REQUEST[email]','$_REQUEST[asunto]', '$_REQUEST[mensaje]')")
         or die("Problemas en el select" . mysqli_error($connection));
     mysqli_close($conexion); 
-    
+    if ($conexion) {
+        ?> 
+        <h3 class = "afirmacion"> ¡Tu mensaje fue enviado!</h3>
+        <?php
+    } else {
+        ?>
+        <h3 class = "error"> ¡Algo salió mal!</h3>
+        <?php
+    }
 ?>
